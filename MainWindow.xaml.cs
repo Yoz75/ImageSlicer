560d2c8ba8fb6bd9ed412a30795c83764a2d2983
yoz75 <levtannn@gmail.com>
2023-12-11 22:00:54 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿
+using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace ImageSlicer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<BitmapSource> Result;
+        private int ResultImageScale;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = "Png file *.png|*.png";
+            if (fileDialog.ShowDialog().Value)
+            {
+                Slicer slicer = new Slicer();
+                slicer.OnException += ShowExceptionMessage;
+                var image = new BitmapImage(new Uri(fileDialog.FileName));
+                Result = slicer.Slice(image, ResultImageScale);
+                MessageTextBlock.Text = $"Opened file:{fileDialog.FileName}";
+            }
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                OpenFolderDialog folderDialog = new OpenFolderDialog();
+                folderDialog.DefaultDirectory = @"C:\Documents";
+                folderDialog.ShowDialog();
+                string pathToSave = folderDialog.FolderName;
+
+                BitmapEncoder encoder = new PngBitmapEncoder();
+                int i = 0;
+                foreach (BitmapSource item in Result)
+                {
+                    encoder = new PngBitmapEncoder();
+                    encoder.Frames.Add(BitmapFrame.Create(item));
+                    using (FileStream stream = new FileStream($"{pathToSave}/{i}.png", FileMode.Create))
+                    {
+                        encoder.Save(stream);
+                    }
+                    i++;
+                }
+                MessageTextBlock.Text = $"Files saved to:{folderDialog.FolderName}";
+            }
+            catch (Exception ex)
+            {
+
+                ShowExceptionMessage(ex);
+            }
+
+        }
+        private void ShowExceptionMessage(Exception ex)
+        {
+            MessageBox.Show($"{ex.Message}\n{ex.StackTrace}", "Oh, there is an error!", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void ResultImagesScaleText_TextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            ResultImageScale = int.Parse(ResultImagesScaleText.Text);
+        }
+    }
+}
---
+++ Slicer.cs
@@ -0,0 +1,36 @@
+﻿
+using System.Windows;
+using System.Windows.Media.Imaging;
+namespace ImageSlicer
+{
+    internal class Slicer
+    {
+        public Action<Exception> OnException;
+        public List<BitmapSource> Slice(BitmapSource sourceImage, int onePartScale)
+        {
+            try
+            {
+                int widthCount = sourceImage.PixelWidth / onePartScale;
+                int heighCount = sourceImage.PixelHeight / onePartScale;
+
+                List<BitmapSource> result = new List<BitmapSource>();
+                CroppedBitmap imagePart;
+                for (int y = 0; y < heighCount; y++)
+                {
+                    for (int x = 0; x < widthCount; x++)
+                    {
+                        imagePart = new CroppedBitmap(sourceImage, new Int32Rect(x * onePartScale, y * onePartScale, onePartScale, onePartScale));
+                        result.Add(imagePart);
+                    }
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                OnException?.Invoke(ex);
+                return null;
+            }
+
+        }
+    }
+}
